76de95a945f71cf090a0df618598938b59f63227
Burak Mike <burakmahyk@gmail.com>
2022-07-21 02:24:45 +0300

    tRPC works fine

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,27 @@
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 import { z } from 'zod'
+import { prisma } from '../../../db/client'
 
-export const appRouter = trpc.router().query('hello', {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `hello ${input?.text ?? 'world'}`,
-    }
-  },
-})
+export const appRouter = trpc
+  .router()
+  .query('hello', {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `hello ${input?.text ?? 'world'}`,
+      }
+    },
+  })
+  .query('getAllQuestions', {
+    async resolve() {
+      return await prisma.question.findMany()
+    },
+  })
 
 // export type definition of API
 export type AppRouter = typeof appRouter
