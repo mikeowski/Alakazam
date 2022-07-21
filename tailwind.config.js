5fa7271a62b825eb80125d6950ec123fbfe12255
Burak Mike <burakmahyk@gmail.com>
2022-07-21 20:44:53 +0300

    activate dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  darkMode: 'media',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
