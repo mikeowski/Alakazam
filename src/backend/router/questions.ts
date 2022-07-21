43312dbb915ed01c54128c5637daff2e3f7954ca
Burak Mike <burakmahyk@gmail.com>
2022-07-21 03:12:07 +0300

    fix tailwind and wrong type imports and expand db question field length

--- src/backend/router/questions.ts
+++ src/backend/router/questions.ts
@@ -2,8 +2,22 @@ import * as trpc from '@trpc/server'
 import { z } from 'zod'
 import { prisma } from '../../db/client'
 
-export const QuestionRouter = trpc.router().query('get-all', {
-  async resolve() {
-    return await prisma.question.findMany()
-  },
-})
+export const QuestionRouter = trpc
+  .router()
+  .query('get-all', {
+    async resolve() {
+      return await prisma.question.findMany()
+    },
+  })
+  .mutation('create', {
+    input: z.object({
+      question: z.string().min(5).max(900),
+    }),
+    async resolve({ input }) {
+      return await prisma.question.create({
+        data: {
+          question: input.question,
+        },
+      })
+    },
+  })
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { withTRPC } from '@trpc/next'
 import superjson from 'superjson'
 import { AppType } from 'next/dist/shared/lib/utils'
-import { AppRouter } from './api/trpc/[trpc]'
+import type { AppRouter } from '../backend/router'
+import '../styles/globals.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
