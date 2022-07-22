d4ec3db95975b31e545cbaa5a7cd0a6ad1fc732e
Burak Mike <burakmahyk@gmail.com>
2022-07-22 16:07:32 +0300

    middleware init

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const res = NextResponse.next()
+}
