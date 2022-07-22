76dee7f8acb68b3f4f53d4fbcb7d93aecf1eec56
Burak Mike <burakmahyk@gmail.com>
2022-07-22 23:29:45 +0300

    some styling

--- src/components/livePolls.tsx
+++ src/components/livePolls.tsx
@@ -6,20 +6,22 @@ const LivePolls: React.FC = () => {
 
   return (
     <div className="flex flex-col justify-center mt-20">
-      <h2 className="text-4xl text-center">Live Polls</h2>
-      <div className="mt-2">
-        {!isLoading && !data ? (
-          <div>No polls found</div>
+      <div>
+        {!isLoading && (!data || data.length == 0) ? (
+          <h2 className="text-4xl text-center">Your dont have a poll yet</h2>
         ) : (
-          data?.map((v) => (
-            <Link key={v.id} href={`poll/${v.id}`}>
-              <a>
-                <div className="border px-4 py-2 rounded-lg mb-3 dark:border-gray-500 hover:translate-x-1 transition-all">
-                  {v.question}
-                </div>
-              </a>
-            </Link>
-          ))
+          <div>
+            <h2 className="text-4xl text-center mb-2">Your Live Polls</h2>
+            {data?.map((v) => (
+              <Link key={v.id} href={`poll/${v.id}`}>
+                <a>
+                  <div className="border px-4 py-2 rounded-lg mb-3 dark:border-gray-500 hover:translate-x-1 transition-all">
+                    {v.question}
+                  </div>
+                </a>
+              </Link>
+            ))}
+          </div>
         )}
       </div>
     </div>
