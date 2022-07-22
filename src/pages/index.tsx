3fe67016ad744cd2bf839e443c50b3f76e9a0fad
Burak Mike <burakmahyk@gmail.com>
2022-07-22 23:14:26 +0300

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,6 @@ import Link from 'next/link'
 import CreatePoll from '../components/createPoll'
 import LivePolls from '../components/livePolls'
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(['questions.get-all'])
-
-  if (isLoading || !data) {
-    return <div>Loading...</div>
-  }
   return (
     <div className="container flex flex-col items-center justify-center px-20 ">
       <CreatePoll />
