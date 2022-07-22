70f953b36f82898dafffb1cc916835ea7b8bcdda
Burak Mike <burakmahyk@gmail.com>
2022-07-22 16:24:01 +0300

    cookie creation in initial request

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,13 @@
-// middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { nanoid } from 'nanoid'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export function middleware(req: NextRequest) {
+  console.log(req.cookies.get('poll-token'))
+  if (req.cookies.has('poll-token')) return
   const res = NextResponse.next()
+  const random = nanoid()
+  res.cookies.set('poll-token', random, { sameSite: 'strict' })
+  return res
 }
