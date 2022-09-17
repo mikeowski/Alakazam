6fd1b15e56f62df8a2f7f4b9730361b87d2fb2d2
Burak Mike <burakmahyk@gmail.com>
2022-09-17 13:58:15 +0300

    feature: added animation to live polls

--- src/components/livePolls.tsx
+++ src/components/livePolls.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { trpc } from '../utils/trpc'
 import { useSession } from 'next-auth/react'
-
+import { motion } from 'framer-motion'
 const LivePolls: React.FC = () => {
   const { data: session } = useSession()
   const { data, isLoading } = trpc.useQuery([
@@ -11,6 +11,25 @@ const LivePolls: React.FC = () => {
   const { data: publicPolls, isLoading: publicIsloading } = trpc.useQuery([
     'questions.get-all-public-questions',
   ])
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  }
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  }
   return (
     <div className="flex flex-col sm:flex-row mt-20 justify-evenly w-full">
       <div>
@@ -19,15 +38,19 @@ const LivePolls: React.FC = () => {
         ) : (
           <div>
             <h2 className="text-2xl text-center mb-2">Your Live Polls</h2>
-            {data?.map((v) => (
-              <Link key={v.id} href={`poll/${v.id}`}>
-                <a>
-                  <div className="border px-4 py-2 rounded-lg mb-3 dark:border-gray-500 hover:translate-x-1 transition-all">
-                    {v.question}
-                  </div>
-                </a>
-              </Link>
-            ))}
+            <motion.ul variants={container} initial="hidden" animate="visible">
+              {data?.map((v) => (
+                <motion.li key={v.id} className="item" variants={item}>
+                  <Link href={`poll/${v.id}`}>
+                    <a>
+                      <div className="border px-4 py-2 rounded-lg mb-3 dark:border-gray-500 hover:translate-x-1 transition-all">
+                        {v.question}
+                      </div>
+                    </a>
+                  </Link>
+                </motion.li>
+              ))}
+            </motion.ul>
           </div>
         )}
       </div>
@@ -38,15 +61,19 @@ const LivePolls: React.FC = () => {
         ) : (
           <div>
             <h2 className="text-2xl text-center mb-2">Public Polls</h2>
-            {publicPolls?.map((v) => (
-              <Link key={v.id} href={`poll/${v.id}`}>
-                <a>
-                  <div className="border px-4 py-2 rounded-lg mb-3 dark:border-gray-500 hover:translate-x-1 transition-all">
-                    {v.question}
-                  </div>
-                </a>
-              </Link>
-            ))}
+            <motion.ul variants={container} initial="hidden" animate="visible">
+              {publicPolls?.map((v) => (
+                <motion.li key={v.id} className="item" variants={item}>
+                  <Link href={`poll/${v.id}`}>
+                    <a>
+                      <div className="border px-4 py-2 rounded-lg mb-3 dark:border-gray-500 hover:translate-x-1 transition-all">
+                        {v.question}
+                      </div>
+                    </a>
+                  </Link>
+                </motion.li>
+              ))}
+            </motion.ul>
           </div>
         )}
       </div>
